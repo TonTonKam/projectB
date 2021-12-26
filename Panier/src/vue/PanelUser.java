a4fbd8b51785826ae66e54c8006414ac3950a4c8
Berkam2 <bertrand.tk@gmail.com>
2021-12-26 18:47:59 +0000

    essai

---
+++ Panier/src/vue/PanelUser.java
@@ -0,0 +1,17 @@
+package vue;
+
+import java.awt.Color;
+
+import javax.swing.JPanel;
+
+public class PanelUser extends JPanel {
+
+	/**
+	 * Create the panel.
+	 */
+	public PanelUser() {
+		this.setBounds(5, 5, 200, 200);
+		this.setBackground(Color.BLUE);
+	}
+
+}
